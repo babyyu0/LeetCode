ab219032cf04746e733dff43989d230eb7675ed3
Zero <58788576+babyyu0@users.noreply.github.com>
2024-05-11 13:17:34 +0900

    Time: 30 ms (47.43%), Space: 45.8 MB (35.97%) - LeetHub

--- 0857-minimum-cost-to-hire-k-workers/0857-minimum-cost-to-hire-k-workers.java
+++ 0857-minimum-cost-to-hire-k-workers/0857-minimum-cost-to-hire-k-workers.java
@@ -2,37 +2,35 @@ import java.util.*;
 
 class Solution {
     public class Worker {
-        Worker(double quality, double wage, double perCost) {
+        Worker(int quality, double perCost) {
             this.quality = quality;
-            this.wage = wage;
             this.perCost = perCost;
         }
-        double quality, wage, perCost;
-        public String toString() {
-            return "Worker {quality=" + quality + ", wage=" + wage + "}";
-        }
+        double quality, perCost;
     }
     
     private double minSum = Double.MAX_VALUE;
     
     public double mincostToHireWorkers(int[] quality, int[] wage, int k) {
-        int n = quality.length;
+        int n = quality.length; //노동자 수
+        double minSum = Double.MAX_VALUE; //최소 금액
         
-        Worker[] workers = new Worker[n];
+        Worker[] workers = new Worker[n]; //임금 기대치를 계산할 노동자 배열
         for(int i = 0; i < n; i++) {
-            workers[i] = new Worker(quality[i], wage[i], (double) wage[i] / quality[i]);
+            workers[i] = new Worker(quality[i], (double) wage[i] / quality[i]);
         }
         
+        //품질에 따른 임금 기대 비율을 오름차순으로 정렬
         Arrays.sort(workers, Comparator.comparingDouble(o -> o.perCost));
-        double qualitySum = 0;
-        PriorityQueue<Double> pq = new PriorityQueue<>(Collections.reverseOrder());
+        double qualitySum = 0; //품질 합계
+        PriorityQueue<Double> pq = new PriorityQueue<>(Collections.reverseOrder()); //품질 내림차순 삽입
         for(int i = 0; i < n; i++) {
-            pq.add(workers[i].quality);
-            qualitySum += workers[i].quality;
+            pq.add(workers[i].quality); //i번째 노동자의 품질 삽입
+            qualitySum += workers[i].quality; //더하기
             
-            if(pq.size() == k) {
-                minSum = Math.min(qualitySum * workers[i].perCost, minSum);
-                qualitySum -= pq.poll();
+            if(pq.size() == k) { //현재까지 Queue에 담긴 k명의 노동자 수
+                minSum = Math.min(qualitySum * workers[i].perCost, minSum); //해당 기대비율에 속하는 k명의 최소임금자 계산
+                qualitySum -= pq.poll(); //가장 높은 품질 (비싼 임금) 제외
             }
         }
         
