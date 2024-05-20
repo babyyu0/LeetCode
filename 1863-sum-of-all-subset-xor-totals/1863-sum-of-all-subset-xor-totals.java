9d5d942890340f23349c6e07b08d06189f33d554
Zero <58788576+babyyu0@users.noreply.github.com>
2024-05-20 14:00:20 +0900

    Time: 1 ms (41.55%), Space: 40.6 MB (82.48%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.java
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.java
@@ -3,9 +3,11 @@ import java.util.Arrays;
 class Solution {
     private int N, answer;
     private int[] nums;
+    private String[][] saved;
     public int subsetXORSum(int[] nums) {
         this.nums = nums;
         N = nums.length;
+        saved = new String[30][30];
         
         combination(0, 0, new int[N]);
         answer += calXOR(nums, N);
@@ -23,22 +25,11 @@ class Solution {
     }
     
     private int calXOR(int[] selected, int count) {
-        String numberStr = Integer.toBinaryString(0);
-        numberStr = "0".repeat(8 - numberStr.length()) + numberStr;
-        String tmp, nextStr;
+        int number = 0;
         for(int i = 0; i < count; i++) {
-            nextStr = Integer.toBinaryString(selected[i]);
-            nextStr = "0".repeat(8 - nextStr.length()) + nextStr;
-            // System.out.println("numberStr:\t" + numberStr);
-            // System.out.println("nextStr:\t" + nextStr);
-            tmp = "";
-            for(int j = 0; j < nextStr.length(); j++) {
-                tmp += numberStr.charAt(j) == nextStr.charAt(j) ? "0":"1";
-            }
-            numberStr = tmp;
+            number = number ^ selected[i];
         }
         
-        // System.out.printf("%s 값의 XOR = %d\n", Arrays.toString(nums), Integer.parseInt(numberStr, 2));
-        return Integer.parseInt(numberStr, 2);
+        return number;
     }
 }
