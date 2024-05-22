26ee169bab8ba58feb17aad7383f282d20276522
Zero <58788576+babyyu0@users.noreply.github.com>
2024-05-22 10:19:16 +0900

    Time: 15 ms (13.66%), Space: 55.5 MB (97.33%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -2,30 +2,36 @@ class Solution {
     List<List<String>> answer;
     public List<List<String>> partition(String s) {
         answer = new ArrayList<>();
-        findPalindrome(new StringBuilder(s), new ArrayList<>());
+        findPalindrome(s, new ArrayList<>());
         
         return answer;
     }
-    private void findPalindrome(StringBuilder sb, List<String> selected) {
-        if(sb.isEmpty()) {
+    private void findPalindrome(String s, List<String> selected) {
+        if(s.isBlank()) {
             answer.add(new ArrayList<>(selected));
-            //System.out.println("* selected : " + selected);
             return;
         }
-        //System.out.printf("문자열 %s 검사:\n", sb);
-        StringBuilder subS, sub1, sub2;
-        for(int i = 1; i <= sb.length(); i++) {
-            subS = new StringBuilder(sb.substring(0, i)); //자른 문자열
+        String subS, sub1, sub2;
+        for(int i = 1; i <= s.length(); i++) {
+            subS = s.substring(0, i); //자른 문자열
             //System.out.printf("자른 문자열 %s 검사:\n", subS);
-            sub1 = new StringBuilder(subS.substring(0, i / 2));
-            sub2 = new StringBuilder(subS.substring(i % 2 == 0 ? i / 2 : i / 2 + 1, i)).reverse();
+            sub1 = subS.substring(0, i / 2);
+            sub2 = reverseStr(subS.substring(i % 2 == 0 ? i / 2 : i / 2 + 1, i));
             //System.out.printf("\t반 자른 문자열 %s와 %s 회문 검사\n", sub1, sub2);
-            if(sub1.toString().equals(sub2.toString()) || subS.length() == 1) {
+            if(sub1.equals(sub2) || subS.length() == 1) {
                 selected.add(subS.toString());
-                findPalindrome(sb.delete(0, i), selected);
-                sb.insert(0, subS);
+                findPalindrome(s.substring(i, s.length()), selected);
                 selected.remove(selected.size() - 1);
             }
         }
+    }
+    
+    private String reverseStr(String s) {
+        StringBuilder result = new StringBuilder();
+        for(int i = s.length() - 1; i >= 0; i--) {
+            result.append(s.charAt(i));
+        }
+        
+        return result.toString();
     }
 }
