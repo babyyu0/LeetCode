4d0d7e384d9ac3ed3240aba7996ffc08172e788e
Zero <58788576+babyyu0@users.noreply.github.com>
2024-05-29 12:54:19 +0900

    Time: 3 ms (20.13%), Space: 41.7 MB (16.77%) - LeetHub

--- 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
@@ -1,32 +1,32 @@
 import java.math.BigInteger;
 
 class Solution {
-    BigInteger two;
+    //BigInteger two;
     public int numSteps(String s) {
-        BigInteger one = new BigInteger("1");
-        two = new BigInteger("2");
-        BigInteger n = calBinary(s);
-        System.out.println(n);
+        int N = s.length();
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        for(int i = 0; i < N; i++) {
+            deque.addLast(s.charAt(i) - '0');
+        }
         
-        int answer = 0;
-        while(n.compareTo(one) > 0) {
-            if(n.mod(two).intValue() == 1) {
-                answer++;
-                n = n.add(one);
-            }
+        int i, count, answer = 0;
+        while(deque.size() != 1) {
             answer++;
-            n = n.divide(two);
+            i = deque.pollLast();
+            if(i == 1) { //홀수일 때
+                count = 0;
+                while(!deque.isEmpty() && deque.pollLast() == 1) {
+                    count++;
+                }
+                deque.addLast(1);
+                while(count-- > 0) {
+                    deque.addLast(0);
+                }
+                deque.addLast(0);
+            }
+            // System.out.println(deque.toString().replace(", ", ""));
         }
         
         return answer;
-    }
-    private BigInteger calBinary(String s) {
-        BigInteger sum = new BigInteger("0");
-        BigInteger bin = new BigInteger("1");
-        for(int i = s.length() - 1; i >= 0; i--) {
-            if(s.charAt(i) == '1') sum = sum.add(bin);
-            bin = bin.multiply(two);
-        }
-        return sum;
     }
 }
