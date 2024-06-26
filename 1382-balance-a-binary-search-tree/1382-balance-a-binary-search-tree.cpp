68eacfb9ec7003aaffcee83e1d3e2a2a7a3bd7a6
Zero <58788576+babyyu0@users.noreply.github.com>
2024-06-26 10:31:30 +0900

    Time: 130 ms (21.81%), Space: 63.5 MB (21.67%) - LeetHub

---
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> nnums;
+    TreeNode* setNodes(int left, int right) {
+        if(left + 1 == right) {
+            return new TreeNode(nnums[right], new TreeNode(nnums[left], NULL, NULL), NULL);
+        } else if (left == right) {
+            return new TreeNode(nnums[right], NULL, NULL);
+        }
+        int mid = (left + right) / 2;
+        return new TreeNode(nnums[mid], setNodes(left, mid - 1), setNodes(mid + 1, right));
+    }
+    TreeNode* balanceBST(TreeNode* root) {
+        vector<TreeNode*> queue;
+        queue.push_back(root);
+        
+        TreeNode* node;
+        while(!queue.empty()) {
+            node = queue[0];
+            queue.erase(queue.begin());
+            
+            nnums.push_back(node->val);
+            cout << node->val << " ";
+            
+            if(node->left != NULL) queue.push_back(node->left);
+            if(node->right != NULL) queue.push_back(node->right);
+        }
+        cout << endl;
+        sort(nnums.begin(), nnums.end());
+        
+        return setNodes(0, nnums.size() - 1);
+    }
+};
