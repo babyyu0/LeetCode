a05ebd0cc39f925cb438ca24966d872e4bc7eb95
Zero <58788576+babyyu0@users.noreply.github.com>
2024-07-23 09:49:02 +0900

    Time: 89 ms (5.20%), Space: 16.7 MB (12.04%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.py
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.py
@@ -1,3 +1,9 @@
 class Solution:
     def frequencySort(self, nums: List[int]) -> List[int]:
-        return sorted(sorted(nums, reverse=True), key=lambda x : nums.count(x))
+        def comparator(a, b):
+            x, y = nums.count(a), nums.count(b)
+            if x == y:
+                return 1 if a < b else -1
+            else:
+                return 1 if x > y else -1
+        return sorted(sorted(nums, reverse=True), key=cmp_to_key(comparator))
