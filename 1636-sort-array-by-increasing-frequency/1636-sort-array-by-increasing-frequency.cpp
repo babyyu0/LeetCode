b5b011e8ba6163e7c0895a6af5cc7c9461e28a29
Zero <58788576+babyyu0@users.noreply.github.com>
2024-07-23 10:06:06 +0900

    Time: 9 ms (34.26%), Space: 14.8 MB (55.33%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
     vector<int> frequencySort(vector<int>& nums) {
-        map<int, int> countMap;
+        unordered_map<int, int> freqMap;
         for(auto& n : nums) {
-            countMap.insert({n, 0});
-            countMap[n]++;
+            freqMap[n]++;
         }
-        vector<pair<int, int>> freqVec(countMap.begin(), countMap.end());
-        sort(freqVec.begin(), freqVec.end(), [](const pair<int, int>& a, const pair<int, int>& b) {
-            return a.second > b.second || (a.second == b.second && a.first < b.first);
+        
+        sort(nums.begin(), nums.end(), [&](int a, int b) {
+            if (freqMap[a] == freqMap[b]) {
+                return a > b;
+            } else {
+                return freqMap[a] < freqMap[b];
+            }
         });
         
-        vector<int> answer(nums.size());
-        int idx = nums.size();
-        for(auto& pair : freqVec) {
-            for(int i = 0; i < pair.second; i++) {
-                answer[--idx] = pair.first;
-            }
-        }
-        
-        return answer;
+        return nums;
     }
 };
