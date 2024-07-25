3536d83a825dbc7ec4e6b7692d4d38de7569f55b
Zero <58788576+babyyu0@users.noreply.github.com>
2024-07-25 09:54:04 +0900

    Time: 1156 ms (45.54%), Space: 25.7 MB (27.20%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,17 +1,14 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        def quicksort(nums: List[int]):
-            if len(nums) == 0 or max(nums) == min(nums):
-                return nums
-            mid = (max(nums) + min(nums)) / 2
-            smallers, biggers = [], []
-            
-            for num in nums:
-                if num < mid:
-                    smallers.append(num)
-                else:
-                    biggers.append(num)
-            
-            return quicksort(smallers) + quicksort(biggers)
-        
-        return quicksort(nums)
+        if len(nums) == 0 or max(nums) == min(nums):
+            return nums
+        mid = (max(nums) + min(nums)) / 2
+        smallers, biggers = [], []
+
+        for num in nums:
+            if num < mid:
+                smallers.append(num)
+            else:
+                biggers.append(num)
+
+        return self.sortArray(smallers) + self.sortArray(biggers)
