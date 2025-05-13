8364f210528ab843cbefb647c1cade040a548354
Zero <58788576+babyyu0@users.noreply.github.com>
2025-05-13 09:31:53 +0900

    Time: 387 ms (15.67%), Space: 13.8 MB (54%) - LeetHub

--- 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.cpp
+++ 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.cpp
@@ -1,37 +1,34 @@
-#include <algorithm>
-#include <set>
-
 class Solution {
 public:
     vector<int> findEvenNumbers(vector<int>& digits) {
-        sort(digits.begin(), digits.end());
         std::set<int> result;
-        vector<bool> visit(digits.size(), false);
-        std::string str;
-        recursion(result, digits, visit, str);
 
-        return vector<int>(result.begin(), result.end());
-    }
+        int digitSize = digits.size();
+        vector<bool> visits(digitSize, false);
 
-private:
-    void recursion(std::set<int>& result, vector<int> digits, vector<bool>& visit, std::string& str) {
-        for(int i = 0; i < digits.size(); i++) {
-            if(str.size() == 0 && digits[i] == 0
-                || str.size() == 2 && digits[i] % 2 == 1) {
+        for(int i = 0; i < digitSize; i++) {
+            if(digits[i] == 0) {
                 continue;
             }
-            if(visit[i] == false) {
-                visit[i] = true;
-                str += to_string(digits[i]);
-                if(str.size() == 3) {
-                    result.insert(stoi(str));
-                } else if(str.size() < 3) {
-                    recursion(result, digits, visit, str);
+            visits[i] = true;
+            for(int j = 0; j < digitSize; j++) {
+                if(visits[j] == true) {
+                    continue;
                 }
-                str.pop_back();
-                visit[i] = false;
+                
+                visits[j] = true;
+                for(int k = 0; k < digitSize; k++) {
+                    if(visits[k] == true || digits[k] % 2 == 1) {
+                        continue;
+                    }
+
+                    result.insert(digits[i] * 100 + digits[j] * 10 + digits[k]);
+                }
+                visits[j] = false;
             }
+            visits[i] = false;
         }
-        return;
+
+        return vector<int>(result.begin(), result.end());
     }
 };
